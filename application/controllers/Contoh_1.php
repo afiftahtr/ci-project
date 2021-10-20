fec71c1f118a4447947aeff88691336fc5f798ec
afiftahtr <afiftahtr08@gmail.com>
2021-10-20 16:59:55 +0700

    Upload Project 3A

---
+++ application/controllers/Contoh_1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh_1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Perkenalkan<br>";
+        echo "Nama saya Afiftah Trengganis, saya seorang mahasiswa Universitas Bina Sarana Informatika<br>";
+        echo "Saya memiliki hobi membuat sajak";
+    }
+}
